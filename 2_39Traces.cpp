dfc680e1fa0f440e88851973fe4377df25bce174
darkstudy45 <114846213+darkstudy45@users.noreply.github.com>
2025-12-13 19:16:09 +0600

    finally accepted

--- 2_39Traces.cpp
+++ 2_39Traces.cpp
@@ -7,11 +7,11 @@ int main (){
 
 
     cout<<"---------------------------------------"<<endl;
-    cout<<"| Roberto                            |"<<endl;
-    cout<<"|                                    |"<<endl;
-    cout<<"|          5786                      |"<<endl; // 5786 - 4 letters so 39-2-4 = 33  | 33-10=23 start at 10th spcae
-    cout<<"|                                    |"<<endl;
-    cout<<"|          UNIFEI                    |"<<endl;
+    cout<<"|         Roberto                     |"<<endl;
+    cout<<"|                                     |"<<endl;
+    cout<<"|         5786                        |"<<endl; // 5786 - 4 letters so 39-2-4 = 33  | 33-10=23 start at 10th spcae
+    cout<<"|                                     |"<<endl;
+    cout<<"|         UNIFEI                      |"<<endl;
     cout<<"---------------------------------------"<<endl;
 
 
