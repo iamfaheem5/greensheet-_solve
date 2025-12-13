ced9246583e71b2f4c9ef4187523ac59e5d253d5
darkstudy45 <114846213+darkstudy45@users.noreply.github.com>
2025-12-13 22:33:48 +0600

    Accepted

---
+++ 4_input.cpp
@@ -0,0 +1,30 @@
+// sloving question https://judge.beecrowd.com/en/problems/view/2759 |2759|
+//
+//
+//
+#include <iostream>
+
+using namespace std;
+
+
+
+
+int main(){
+
+    char x , y , z ;
+    cin>>x;
+    cin>>y;
+    cin>>z;
+
+    cout<<"A = "<<x<<", "<<"B = "<<y<<", "<<"C = "<<z<<""<<endl;
+    cout<<"A = "<<y<<", "<<"B = "<<z<<", "<<"C = "<<x<<""<<endl;
+    cout<<"A = "<<z<<", "<<"B = "<<x<<", "<<"C = "<<y<<""<<endl;
+
+
+
+
+
+    return 0;
+
+
+}
